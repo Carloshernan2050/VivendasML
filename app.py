62eb783e008f71b7d802f734cba4e1cd2c5ae56a
Carlos Hernan Molina Arenas <Carloshernan2050@gmail.com>
2025-07-08 10:21:50 -0500

    añadido el codigo a app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+from  flask import Flask
+from controllers.vivienda_controller import vivienda_blueprint
+
+app = Flask(__name__, template_folder='views')
+app.register_blueprint(vivienda_blueprint')
+                       
+if __name__ == '__main__':
+    app.run(debug=True)
+
